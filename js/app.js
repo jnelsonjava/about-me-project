54b9f417aaeaea40587508ca8df15e485cd6e71e
Jack Nelson <jnelson.java@gmail.com>
2020-07-07 15:13:46 -0700

    planned JS structure and created first user question

---
+++ js/app.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/*
+Goal is to ask the user 5 yes/no questions and respond to the user with alerts
+that are responsive to the user input.
+Secondary goal is to make a responsive list that displays the information the user requested.
+
+5 questions:
+What's the user's name?
+Would the user like to know where I'm from?
+Would the user like to know about my education?
+Would the user like to know where I worked?
+Would the user like to know about my hobbies?
+Would the user like to know about my pet?
+
+They don't really have a choice on that last one,
+I'm going to serve them my cat image...
+
+The responses to the questions will be along the lines of
+'Thanks for showing interest, I'll add that to the page.'
+'It's ok, maybe there's something else that would interest you?'
+*/
+
+var userName = prompt('Welcome to a page about the author of the page. First, what\'s your name?');
+var welcomeMessage = 'Welcome, ' + userName + '! I just have a few questions to make sure you get the information you want.';
+
+console.log(welcomeMessage);
+// alert(welcomeMessage);
+
+
+
+
+
+
+
+
