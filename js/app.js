63b9802f44a7a957d8eefd5be270e26e6d05784b
Jack Nelson <jnelson.java@gmail.com>
2020-07-07 15:45:30 -0700

    updated js with two more questions

--- js/app.js
+++ js/app.js
@@ -28,7 +28,40 @@ console.log(welcomeMessage);
 // alert(welcomeMessage);
 
 
+// saving a message to serve the user in case their responses weren't valid
+var failedAnswerMessage = 'I\'m sorry, I didn\'t understand. Make sure to answer "y" for yes or "n" for no. I\'ll assume that was a yes and move on.';
 
+
+// found how to use .toLowerCase() here: https://www.w3schools.com/jsref/jsref_tolowercase.asp
+var userInterestedInBackground = prompt('Would you like to know about where I\'m from? (y/n)').toLowerCase();
+
+var backgroundMessage;
+if (userInterestedInBackground === 'y') {
+  backgroundMessage = 'Thanks for your interest. I\'ll add that to the page.';
+} else if (userInterestedInBackground === 'n') {
+  backgroundMessage = 'That\'s fine, let\'s see if there\'s something else you would like to know.';
+} else {
+  backgroundMessage = failedAnswerMessage;
+}
+
+console.log(backgroundMessage);
+// alert(backgroundMessage);
+
+
+
+var userInterestedInEducation = prompt('Would you like to know about my education? (y/n)').toLowerCase();
+
+var educationMessage;
+if (userInterestedInEducation === 'y') {
+  educationMessage = 'Great! Coming right up.';
+} else if (userInterestedInEducation === 'n') {
+  educationMessage = 'You know, maybe that\'s for the best. Hmmm, what\'s next...';
+} else {
+  educationMessage = failedAnswerMessage;
+}
+
+console.log(educationMessage);
+// alert(educationMessage);
 
 
 
